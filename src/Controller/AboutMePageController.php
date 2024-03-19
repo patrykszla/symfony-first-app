3adcdcb7fc50f3d7aefe2e4bde9ec04cedd9d161
Patryk Szlachta <patryk.szla@interia.pl>
2024-03-19 21:37:43 +0100

    work on page about me

--- src/Controller/AboutMePageController.php
+++ src/Controller/AboutMePageController.php
@@ -2,15 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\InformationAboutMeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AboutMePageController extends AbstractController
 {
+    public function __construct(
+        private InformationAboutMeRepository $informationAboutMeRepository
+    )
+    {
+    }
     #[Route('/about-me', name: 'about-me')]
     public function index(): Response
     {
-        return $this->render('about_me_page/index.html.twig');
+        $informations = $this->informationAboutMeRepository->findAll();
+        // dd($informations);
+        return $this->render('about_me_page/index.html.twig', ['informations' => $informations]);
     }
 }
